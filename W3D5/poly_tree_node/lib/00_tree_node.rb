d7bf6e309978767e7498aa7eb4aed3598c8d720a
BrandonKim <brandonkim@virginia.edu>
2020-06-01 11:33:32 -0400

    Created KnightPathFinder class and edited Poly_Tree_Node class

--- W3D5/poly_tree_node/lib/00_tree_node.rb
+++ W3D5/poly_tree_node/lib/00_tree_node.rb
@@ -54,7 +54,7 @@ class PolyTreeNode
 
 
     
-end
+# end
 
     #         1
     #      /     \
@@ -62,3 +62,128 @@ end
     #   /  \     /  \
     #  4   5    6    7 
 
+
+
+
+
+
+# module Searchable
+# #   I wrote this as a mixin in case I wanted to later write another
+# #   TreeNode class (e.g., BinaryTreeNode). All I need is `#children` and
+# #   `#value` for `Searchable` to work.
+
+#   def dfs(target = nil, &prc)
+#     raise "Need a proc or target" if [target, prc].none?
+#     prc ||= Proc.new { |node| node.value == target }
+
+#     return self if prc.call(self)
+
+#     children.each do |child|
+#       result = child.dfs(&prc)
+#       return result unless result.nil?
+#     end
+
+#     nil
+#   end
+
+
+#     #         1
+#     #      /     \
+#     #     2       3
+#     #   /  \     /  \
+#     #  4   5    6    7 
+
+    
+#   def bfs(target = nil, &prc) #3
+#     raise "Need a proc or target" if [target, prc].none?
+#     prc ||= Proc.new { |node| node.value == target }
+
+#     nodes = [self] 
+#     until nodes.empty?
+#       node = nodes.shift
+
+#       return node if prc.call(node)
+#       nodes.concat(node.children)
+#     end
+
+#     nil
+#   end
+
+#   def count
+#     1 + children.map(&:count).inject(:+)
+#   end
+# end
+
+# class PolyTreeNode
+#   include Searchable
+
+#   attr_accessor :value
+#   attr_reader :parent
+
+#   def initialize(value = nil)
+#     @value, @parent, @children = value, nil, []
+#   end
+
+#   def children
+#     # We dup to avoid someone inadvertently trying to modify our
+#     # children directly through the children array. Note that
+#     # `parent=` works hard to make sure children/parent always match
+#     # up. We don't trust our users to do this.
+#     @children.dup
+#   end
+
+#   def parent=(parent)
+#     return if self.parent == parent
+
+#     # First, detach from current parent.
+#     if self.parent
+#       self.parent._children.delete(self)
+#     end
+
+#     # No new parent to add this to.
+#     @parent = parent
+#     self.parent._children << self unless self.parent.nil?
+
+#     self
+#   end
+
+#   def add_child(child)
+#     # Just reset the child's parent to us!
+#     child.parent = self
+#   end
+
+#   def remove_child(child)
+#     # Thanks for doing all the work, `parent=`!
+#     if child && !self.children.include?(child)
+#       raise "Tried to remove node that isn't a child"
+#     end
+
+#     child.parent = nil
+#   end
+
+#   protected
+  
+#   # Protected method to give a node direct access to another node's
+#   # children.
+#   def _children
+#     @children
+#   end
+# end
+
+# # a = PolyTreeNode.new(1)
+# # b = PolyTreeNode.new(2)
+# # c = PolyTreeNode.new(3)
+# # d = PolyTreeNode.new(4)
+# # e = PolyTreeNode.new(5)
+# # f = PolyTreeNode.new(6)
+# # g = PolyTreeNode.new(7)
+
+# # a.add_child(b)
+# # a.add_child(c)
+# # b.add_child(d)
+# # b.add_child(e)
+# # c.add_child(f)
+# # c.add_child(g)
+
+
+# # p a.count
