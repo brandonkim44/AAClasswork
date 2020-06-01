7cdb14d872146d7cda28b87d6aefe0840d294bee
BrandonKim <brandonkim@virginia.edu>
2020-06-01 17:39:33 -0400

    Created Tic Tac Toe Node

--- W4D1/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ W4D1/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -1,10 +1,16 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
+    
   end
 
   def winning_node?(evaluator)
@@ -13,5 +19,18 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children = []
+    (0...@board.rows.length).each do |row|
+      (0...@board.rows.length).each do |col|
+        pos = [row, col]
+        if @board.empty?(pos) #probably need another if statement here to decide which marker we are going to use. like if marker == x make it o and vice a versa.
+          dup_board = self.board.dup
+          dup_board[pos] = self.next_mover_mark 
+          children << TicTacToeNode.new(dup_board, :o, pos)
+        end
+      end
+    end
+
+    children
   end
 end
