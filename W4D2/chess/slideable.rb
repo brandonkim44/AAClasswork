1863b779d5be4a475c43b1fe161bb70c57279031
BrandonKim <brandonkim@virginia.edu>
2020-06-03 17:32:11 -0400

    Added to slideable module

--- W4D2/chess/slideable.rb
+++ W4D2/chess/slideable.rb
@@ -24,10 +24,37 @@ module Slideable
 
     def moves
         possible_moves = []
-        move_dirs.each
+        move_dirs.each do |dir|
+            dx, dy = dir
+            possible_moves += grow_unblocked_moves_in_dir(dx,dy)
+        end
     end
+
+    private 
 
     def move_dirs
         raise NotImplementedError
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy) #dx=+1, dy=0 => right
+        collected_moves = []
+        current_pos = self.pos
+        new_pos = [current_pos[0]+dx, current_pos[1]+dy]
+        keep_moving = true
+        while keep_moving
+            if valid_pos?(new_pos) && new_pos.empty? 
+                collected_moves << new_pos
+                current_pos = new_pos 
+                new_pos = [new_pos[0]+dx, new_pos[1]+dy] 
+            elsif valid_pos?(new_pos) && !new_pos.empty?
+                if board[current_pos].color != board[new_pos].color
+                    collected_moves << new_pos
+                    keep_moving = false
+                else
+                    keep_moving = false
+                end
+            end
+        end
+        collected_moves
     end
 end
