845b4a50098a1d37458e28f036368db7ed0c7e62
BrandonKim <brandonkim@virginia.edu>
2020-06-16 15:41:40 -0400

    Completed up to initialize method

--- W6D2/build_your_own_active_record/lib/01_sql_object.rb
+++ W6D2/build_your_own_active_record/lib/01_sql_object.rb
@@ -4,19 +4,35 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+  
   def self.columns
-    # ...
+    return @columns if @columns
+    array_of_colnames_and_records = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    @columns = array_of_colnames_and_records[0].map { |column_name| column_name.to_sym }
   end
 
   def self.finalize!
+    self.columns.each do |column_name|
+      define_method(column_name) do 
+        attributes[column_name]
+      end
+      define_method("#{column_name}=") do |value| #is a class method itself but makes instance methods
+        attributes[column_name] = value #attributes key = column_name, and value is value inside the column
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+      instance_variable_set("@#{self}", "#{table_name.to_s.downcase}s")
   end
 
   def self.table_name
-    # ...
+    "#{self.to_s.downcase}s"
   end
 
   def self.all
@@ -32,15 +48,15 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    @attributes.values
   end
 
   def insert
