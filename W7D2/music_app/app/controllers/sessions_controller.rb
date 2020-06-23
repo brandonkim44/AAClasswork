e3384e7a81b37aa6fbf5f00a2d83a8f0abe890bb
BrandonKim <brandonkim@virginia.edu>
2020-06-23 15:16:08 -0400

    added to views and controllers

--- W7D2/music_app/app/controllers/sessions_controller.rb
+++ W7D2/music_app/app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
     end
 
     def create #actually logs the user in and creates a new session (logging in means session_tokens must match)
-        user = User.find_by_credentials(
+        user = User.find_by_credentials( #can we use an instance variable with @ here or is it unnecessary?********* ask TA
             params[:user][:email],
             params[:user][:password]
         ) #after user enters data on form, the params now has nested keys of :email, and :password and we can use our method in UserModel
         
         if user #if the user exists, then log them in (set tokens to match), else render :new with flash error
-            session[:session_token] = user.reset_session_token! #we use reset_session_token! here too make sure any old session token is updated (even though it technically generates a new one when we create user)
+            log_in_user!(user)
             redirect_to user_url(user) # User#show page
         else
             flash.now[:errors] = ['Invalid Login Credentials']
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy #logs user out of session (session tokens shouldn't match)
-        user.session_token = User.reset_session_token!
+        current_user.reset_session_token! if current_user #if the current user exists (user is logged in), then reset_session_token for user
         session[:session_token] = nil
         redirect_to new_session_url #redirects to login page
     end
